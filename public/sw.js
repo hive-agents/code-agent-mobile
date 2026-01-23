bd9a19f59fbaf4f0b855bba06271adbb82dca820
Nick Ludwig <hosted-fornet@proton.me>
2026-01-23 22:10:18 +0000

    make pwa

---
+++ public/sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', () => {
+  self.skipWaiting()
+})
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting()
+  }
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request))
+})
